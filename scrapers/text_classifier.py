5bd59fa837fb278012adda3769fc385bb4d5624d
jfreek <jorgee.abud@gmail.com>
2017-06-03 01:29:09 -0500

    creating new file for the classifier algorithm

---
+++ scrapers/text_classifier.py
@@ -0,0 +1,6 @@
+import nltk
+from nltk.corpus import movie_reviews
+
+documents = [(list(movie_reviews.words(fileid)), category)
+             for category in movie_reviews.categories()
+             for fileid in movie_reviews.fileids(category)]
