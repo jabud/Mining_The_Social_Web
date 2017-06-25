9f3acd96e32364a65d4f17a88a7edff5a64fc535
jfreek <jorgee.abud@gmail.com>
2017-06-24 22:23:05 -0600

    implementing svm classifier

--- training.py
+++ training.py
@@ -1,9 +1,14 @@
 # -*- coding: UTF-8 -*-
 import pandas as pd
-import joblib
-from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 import re
+import time
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import svm
+from sklearn.pipeline import Pipeline
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
 
 
 def replace_text(text, replace_list, replace_by):
@@ -48,8 +53,33 @@ stop_words = set(stopwords.words('english'))
 # initialize model to vectorize
 vec = TfidfVectorizer(lowercase=True, use_idf = True, norm=None, smooth_idf=False, 
 	analyzer='word', input='content', stop_words=stop_words, min_df=10, max_features=5000)
-# train vectoriuzer
+# train vectorizer
 vec.fit_transform(doclist)
-# save the model
+# save vectorizer
 filename = '/home/jfreek/workspace/Mining_The_Social_Web/models/tfidfsw.sav'
 joblib.dump(vec, filename)
+# SVM classifier
+svm_clf =svm.LinearSVC(C=0.1)
+vec_svm = Pipeline([('vectorize', vec), ('svm', svm_clf)])
+
+# Training data
+X = trainset_df['text'].values
+y = trainset_df['category'].values
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=6)
+
+t0 = time.time()
+vec_svm.fit(x_train, y_train)
+t1 = time.time()
+total = t1 - t0
+print "total time: " + str(total)
+
+# get average accuracy
+result = vec_svm.score(x_test, y_test)
+# predict 0 or 1 Conf Matrix
+y_pred = vec_svm.predict(x_test)
+confusion_m = confusion_matrix(y_test, y_pred)
+# show results:
+print result
+print confusion_m
+print(classification_report(y_test, y_pred))
+
