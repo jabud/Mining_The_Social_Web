dc080bd0f6972c3886ce760d8a29aba0da448583
jfreek <jorgee.abud@gmail.com>
2017-08-14 18:20:42 -0600

    adding attributes image, url, price

--- scrapers/amazon_scraper.py
+++ scrapers/amazon_scraper.py
@@ -1,7 +1,7 @@
 import amazonproduct
+import pandas as pd
 
 # TODO: Get price, title, image(s), link to buy and structure it.
-# TODO: Paginate through at least 10 (maybe more)
 # TODO: Scrape review's urls :( 
 
 
@@ -17,8 +17,26 @@ class AmazonScraper:
 										'secret_key': config['AWS_SECRET_KEY'], 
 										'associate_tag': config['AWS_ASSOCIATE_TAG'],
 										'locale': 'mx'})
-items = api.item_search(search_index='Books', ResponseGroup='Reviews', Title="Quantum Theory Of Fields")
-urls = []
-for item in items:
-    if item.CustomerReviews.HasReviews:
-        urls.append(item.CustomerReviews.IFrameURL.text)
+
+    def scrape_items(self, category, search):
+		items = self.api.item_search(search_index=category, ResponseGroup='Large', Title=search)
+		df = pd.DataFrame()
+		for item in items:
+			results = {}
+			results["price"] = unicode(item.ItemAttributes.ListPrice.FormattedPrice) if hasattr(item.ItemAttributes, 'ListPrice') else None
+			results["image"] = item.LargeImage.URL.text if hasattr(item.LargeImage, 'LargeImage') else None
+			results["url"] = item.CustomerReviews.IFrameURL.text if item.CustomerReviews.HasReviews else None
+			df=df.append(other=results, ignore_index=True)
+		return df
+
+def main():
+    # initialize amazon class
+    amz = AmazonScraper()
+    # scrape items
+    search = 'Feynman Lectures'
+    category = "Books"
+    df = amz.scrape_items(category=category, search=search)
+
+
+if __name__ == "__main__":
+    main()
