626c9f1332487d7cd01ae34d977f631cadfedc39
jfreek <jorgee.abud@gmail.com>
2017-09-08 00:49:04 -0500

    writing down some ideas

--- text_classifier.py
+++ text_classifier.py
@@ -34,11 +34,23 @@ def main():
 	# load the model
 	filename = '/Mining_The_Social_Web/models/svmtfidf.sav'
 	clf = joblib.load(filename)
+	# classify
 	df['category'] = df['text'].map(lambda x: clf.predict([x])[0] if x else -1)
 	pos = df['category'].value_counts(normalize=True)[1]
 	neg = df['category'].value_counts(normalize=True)[0]
+	# print results
 	print ('positive %: ' + str(pos))
 	print ('negative %: ' + str(neg))
+	# to csv
+	tocsv=pd.DataFrame(data={"company": account, "positive": pos, "negative": neg}, index=[0])
+	# save csv
+	tocsv.to_csv(path_or_buf='/Mining_The_Social_Web/tmp/pepsico.csv', 
+		header=['company', 'positive', 'negative'],
+        columns=['company', 'positive', 'negative'], index=None, sep=',', mode='w')
+	# df = pd.read_csv('/Mining_The_Social_Web/tmp/cocacola.csv', 
+ #                    sep=',', header=0, names=['company', 'positive', 'negative'])
+	# get top 10 accounts
+	df.screen_name.value_counts().head(10)
 
 
 if __name__ == '__main__':
